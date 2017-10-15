737515e89c54943ad54b9906efb88fd3c858f62d
yul lee kim <yul@YLui-MacBook.local>
2017-10-14 17:17:13 -0700

    second commit

--- app/routes/subreddits.js
+++ app/routes/subreddits.js
@@ -1,13 +1,11 @@
+/* global $ */
 import Ember from 'ember';
 
 export default Ember.Route.extend({
     model(params) {
-      // console.log(params.subreddit);
-      // console.log(subreddit);
 
       let subreddit = params.subreddit;
       let url = `https://www.reddit.com/r/${subreddit}.json`;
-      // let url =`http://api-eventful.herokuapp.com/api/events?keywords=${subreddit}`;
 
       return $.getJSON(url);
     }
